30ec3f4bb982c31dba5b0149f5d80c2baee46456
Your Name <ekitanga96@gmail.com>
2024-10-03 10:41:51 +0300

    first commit

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <=25; i++){
+    console.log ("")
+    console.log ("")
+    console.log ("Multiplication table for", i)
+    for (let j = 1; j<=25; j++){
+        let product = i * j;
+        console.log (i +"X"+ j + "=" + product )
+    }
+}
+
